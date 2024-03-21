1b66bb9bc119f5a707fdc729056ea2e48f68482c
iconew123 <iconew123@gmail.com>
2024-03-21 15:48:09 +0900

    Initial Commit

---
+++ shop/src/shop/Main.java
@@ -0,0 +1,25 @@
+package shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Shop shop = new Shop("Mega");
+		shop.run();
+
+		// Shop Project
+		// class : Item , ItemManager , User , UserManager , Shop , Main , FileManager ,
+		// Cart
+		// method :
+		// 유저 -
+		// ㄴ 회원가입 / 탈퇴
+		// ㄴ 로그인 / 로그아웃
+		// ㄴ 쇼핑하기
+		// ㄴ 마이페이지 (내 장바구니 , 항목삭제 , 수량수정 , 결제)
+		// ㄴ 자동저장 / 자동로드
+
+		// 관리자 -
+		// ㄴ 아이템 등록 / 삭제 / 수정
+		// ㄴ 조회(총 매출)
+	}
+
+}
